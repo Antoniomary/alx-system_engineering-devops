43e40606a24a07a4c84820d4196c28037a89e38d
Antoniomary <amanso.totti@yahoo.com>
2024-03-23 03:19:36 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # This manifest installs flask from pip3
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
+}
+
+# gets werkzerg v. 2.1.1 as it can work with the above flask version
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
 }
