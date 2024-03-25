892fd4e9f5130fbdf95769d37dc387da7b343f11
Antoniomary <amanso.totti@yahoo.com>
2024-03-24 17:20:39 -0700

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,23 +1,14 @@
 # This manifest set up your client SSH configuration file
 # so that you can connect to a server without typing a password
-file { '~/.ssh/config':
-  ensure => present,
-}
-
-file_line { 'host name':
-  ensure => present,
-  path   => '~/.ssh/config',
-  line   => 'Host 35.153.232.249',
-}
 
 file_line { 'private key':
   ensure => present,
-  path   => '~/.ssh/config',
+  path   => '/etc/ssh/ssh_config',
   line   => '        PasswordAuthentication no',
 }
 
 file_line { 'file to read':
   ensure => present,
-  path   => '~/.ssh/config',
+  path   => '/etc/ssh/ssh_config',
   line   => '        IdentityFile ~/.ssh/school',
 }
