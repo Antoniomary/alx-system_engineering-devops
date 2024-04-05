3c471f94a9f30ca9b8a1223adb59a747acd1c0fd
Antoniomary <amanso.totti@yahoo.com>
2024-04-05 08:35:19 -0700

    7-puppet_install_nginx_web_server.pp

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,34 @@
+# This manifest installs and configures Nginx to
+#      -> liston on port 80
+#      -> perform a 301 redirect when querying /redirect_me.
+
+package { 'nginx':
+  ensure => installed,
+}
+
+exec { 'ufw_allow_http':
+  command  => "sudo ufw allow 'Nginx HTTP'",
+  require  => Package['nginx'],
+  provider => shell,
+}
+
+exec { 'ufw_allow_tcp':
+  command  => 'sudo ufw allow ssh',
+  require  => Package['nginx'],
+  provider => shell,
+}
+
+file_line { 'redirection':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default':
+  line   => 'rewrite /redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
+}
+
+service { 'ufw':
+  ensure => running,
+  enable => true,
+} -> Package['nginx']
+
+service { 'nginx':
+  ensure => running,
+}
