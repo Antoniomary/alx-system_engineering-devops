d99f76b022c02305e502430f4f393a364e8d8248
Antoniomary <amanso.totti@yahoo.com>
2024-04-06 03:42:25 -0700

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -11,3 +11,19 @@ package { 'nginx':
   ensure  => present,
   require => Exec['update'],
 }
+
+exec { 'index_html':
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html
+  provider => shell,
+  require => Package['nginx'],
+}
+
+file_line { 'redirection':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default':
+  line   => 'rewrite /redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+service { 'nginx':
+  ensure  => running,
+}
