8ad21a0a01975cc8caa4fbe48c142d31817a827f
Antoniomary <amanso.totti@yahoo.com>
2024-06-19 19:35:47 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,19 @@ def number_of_subscribers(subreddit):
     """a function that queries the Reddit API and returns
        the number of subscribers
     """
-    res = get(f'https://www.reddit.com/r/{subreddit}/about.json')
-    res = res.json()
-    res = res.get('data').get('subscribers')
+    user_agent = {'User-Agent': 'Google Chrome Version 81.0.4044.1    29'}
+    try:
+        res = get(f'https://www.reddit.com/r/{subreddit}/about.json',
+                  headers=user_agent)
+        if res.status_code != 200:
+            return 0
 
-    if res is None:
+        data = res.json().get('data')
+        if data is None:
+            return 0
+
+        subscribers = data.get('subscribers')
+        return subscribers if subscribers is not None else 0
+
+    except Exception as e:
         return 0
-
-    return res
